259ba12a3e6f04c636031b7e40f9b35cbc5de654
Johan Borgström <johan@johans-mac-pro.lan>
2014-10-21 23:56:53 +0200

    update nodes2 expr

--- add_nodes2_expr.jsx
+++ add_nodes2_expr.jsx
@@ -1,5 +1,3 @@
-#target aftereffects
-
 var proj = app.project; 
 var comp = proj.activeItem; 
 var sel = comp.selectedLayers; 
@@ -16,19 +14,21 @@ function add_nodes2_exp()
         return;
     }
 
+    var exp_pos_xy = "p = thisComp.layer(\"" + source.name +"\").transform.position;\n[p[0], p[1]]";
+    var exp_pos_z = "thisComp.layer(\"" + source.name +"\").transform.position[2]";
 
-    var exp_pos = "thisComp.layer(\"" + source.name +"\").transform.position";
-    var exp_rot_x = "thisComp.layer(\"" + source.name +"\").transform.xRotation";
-    var exp_rot_y = "thisComp.layer(\"" + source.name +"\").transform.yRotation";
-    var exp_rot_z = "thisComp.layer(\"" + source.name +"\").transform.zRotation";
-
-    //$.writeln(exp)
-    //target.property("position").expression = exp;
-    target.property("xRotation").expression = exp_rot_x;
-    target.property("yRotation").expression = exp_rot_y;
-    target.property("zRotation").expression = exp_rot_z;
+    var exp_rot_x = "thisComp.layer(\"" + source.name +"\").rotationX + " + "thisComp.layer(\"" + source.name +"\").orientation[0];";
+    var exp_rot_y = "thisComp.layer(\"" + source.name +"\").rotationY + " + "thisComp.layer(\"" + source.name +"\").orientation[1];";
+    var exp_rot_z = "thisComp.layer(\"" + source.name +"\").rotationZ + " + "thisComp.layer(\"" + source.name +"\").orientation[2];";
     
-    //alert("Added expressions")
+    target.effect("Nodes 2")("Position").expression = exp_pos_xy;
+    target.effect("Nodes 2")("Position Z").expression = exp_pos_z;
+    
+    target.effect("Nodes 2")("Rotation X").expression = exp_rot_x;
+    target.effect("Nodes 2")("Rotation Y").expression = exp_rot_y;
+    target.effect("Nodes 2")("Rotation Z").expression = exp_rot_z;
+
 }
 
 add_nodes2_exp()
+
