2482bd6f1e402eee4b24017ae634b38f1ecfda17
Johan Borgström <johan@petfactory.se>
2015-06-25 21:19:06 +0200

    add output templates

--- rendering/render_queue/dev/select_frames_ui.jsx
+++ rendering/render_queue/dev/select_frames_ui.jsx
@@ -1,3 +1,18 @@
+function getOutputModuleTemplateNames()  
+{  
+      var currentOMName;  
+      var OMStringsPrefSection = "Output Module Spec Strings Section v28";  
+      var OMStringsKeyPrefix = "Output Module Spec Strings Name ";  
+      var hiddenPrefixRE = /_HIDDEN/;  
+      var OMList = new Array();  
+      for(var i = 0; app.preferences.havePref(OMStringsPrefSection, OMStringsKeyPrefix+i, PREFType.PREF_Type_MACHINE_INDEPENDENT_OUTPUT); i++)  
+      {  
+        currentOMName = app.preferences.getPrefAsString(OMStringsPrefSection, OMStringsKeyPrefix+i, PREFType.PREF_Type_MACHINE_INDEPENDENT_OUTPUT);  
+        if(currentOMName.match(hiddenPrefixRE)==null) OMList.push(currentOMName);  
+      }  
+      return OMList;  
+}
+
 function apa()
 {
     var frame_string = frame_edittext.text;
@@ -20,13 +35,22 @@ function apa()
     for (var j = 0; j < num_frames; j++)
     {
         //$.writeln(frame_list[j]);
-        alert(frame_list[j]);
+        template = typeDropdown.selection.index;
+        //alert(frame_list[j]);
+        alert(template);
+        
     }
 }
 var myWindow = new Window ("dialog", "Form");
 myWindow.orientation = "row";
 myWindow.add ("statictext", undefined, "Frames:");
+
 var frame_edittext = myWindow.add ("edittext", undefined, "John");
+
+var template_list = getOutputModuleTemplateNames()
+var typeDropdown = myWindow.add ("dropdownlist", undefined, template_list);
+typeDropdown.selection = 2;
+
 //myText.active = true;
 frame_edittext.characters = 20;
 render_button = myWindow.add ("button", undefined, "Render Frames");
