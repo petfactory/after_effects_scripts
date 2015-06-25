f9fa66da1ac4260375bfd38d3febbc9937b95c3c
Johan Borgström <johan@petfactory.se>
2015-06-25 21:45:25 +0200

    add dir

--- rendering/render_queue/dev/select_frames_ui.jsx
+++ rendering/render_queue/dev/select_frames_ui.jsx
@@ -13,6 +13,16 @@ function getOutputModuleTemplateNames()
       return OMList;  
 }
 
+function set_dir()
+{
+    dir = Folder.selectDialog('Please select the folder to be imported:', Folder('~/Documents/projects/the_bot/bot_illustrator/'));
+
+    if (dir)
+    {
+        dir_edittext.text = dir;
+    }
+}
+
 function apa()
 {
     var frame_string = frame_edittext.text;
@@ -31,28 +41,42 @@ function apa()
        }
     }
     
+    template_name = typeDropdown.selection.text;
+    
     var num_frames = frame_list.length;
     for (var j = 0; j < num_frames; j++)
     {
         //$.writeln(frame_list[j]);
-        template = typeDropdown.selection.index;
         //alert(frame_list[j]);
-        alert(template);
+        start_frame = frame_list[j];
+        alert(template_name + " - " + start_frame + " - " + dir_edittext.text);
         
     }
 }
 var myWindow = new Window ("dialog", "Form");
-myWindow.orientation = "row";
+myWindow.orientation = "column";
 myWindow.add ("statictext", undefined, "Frames:");
 
-var frame_edittext = myWindow.add ("edittext", undefined, "John");
+var frame_edittext = myWindow.add ("edittext", undefined, "0, 25, 50");
+//myText.active = true;
+frame_edittext.characters = 20;
+
+var dir_edittext = myWindow.add ("edittext", undefined, "Directory");
+ dir_edittext.active = false;
+dir_edittext.characters = 20;
+
+var set_dir_button = myWindow.add ("button", undefined, "Set Directory");
+set_dir_button.onClick = set_dir;
 
 var template_list = getOutputModuleTemplateNames()
 var typeDropdown = myWindow.add ("dropdownlist", undefined, template_list);
 typeDropdown.selection = 2;
 
-//myText.active = true;
-frame_edittext.characters = 20;
+
 render_button = myWindow.add ("button", undefined, "Render Frames");
 render_button.onClick = apa;
+
 myWindow.show ();
+
+
+
