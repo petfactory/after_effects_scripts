a9417889c304cd22e083f40da81b2db775498ea4
Johan Borgström <johan@petfactory.se>
2015-07-02 16:03:45 +0200

    add xpr string

--- animation/shape_remap_expr/add_expr_to_properties.jsx
+++ animation/shape_remap_expr/add_expr_to_properties.jsx
@@ -1,44 +1,1 @@
-var comp = app.project.activeItem;
-
-//var null_ctrl = comp.layers.addSolid([0,0,0], "Solid", comp.width, comp.height, 1);
-//null_ctrl.name = null_name;
-//null_ctrl.Effects.addProperty("ADBE Circle");    
-//null_ctrl.Effects.addProperty("Circle"); // also works
-
-function build_eval_string(layer, prop_array, expr)
-{
-    var layer = layer;
-    var eval_string = "layer";
-
-    for (j = 0; j < prop_array.length; j++)
-    {
-        eval_string += '.property("'+prop_array[j]+'")';
-    }
-    eval_string += '.expression = "' +expr+'"';
-       
-    try
-    {
-        
-        eval(eval_string)
-    }
-    catch(e)
-    {
-        alert("Could not add expression\n" + String(e))
-    }
-}
-
-function add_to_layers(sel_layers)
-{
-    alert(sel_layers.length)
-    for (i = 0; i < sel_layers.length; i++)
-    {
-        build_eval_string(sel_layers[i], ["Effects", "Circle", "Radius"], "time*100") 
-    }
-}
-
-var sel_layers = comp.selectedLayers;
-add_to_layers(sel_layers);
-
-//build_eval_string(null_ctrl, ["Effects", "Circle", "Radius"], "time*100")
-
-
+﻿var comp = app.project.activeItem;//var null_ctrl = comp.layers.addSolid([0,0,0], "Solid", comp.width, comp.height, 1);//null_ctrl.name = null_name;//null_ctrl.Effects.addProperty("ADBE Circle");    //null_ctrl.Effects.addProperty("Circle"); // also worksfunction build_eval_string(layer, prop_array, expr){    var layer = layer;    var eval_string = 'layer';    for (j = 0; j < prop_array.length; j++)    {        eval_string += '.property("'+prop_array[j]+'")';    }    eval_string += ".expression = \'"+ expr +"\'";    //eval_string += expr    //alert(eval_string)    try    {        eval(eval_string)        return null;    }    catch(e)    {        return layer;    }}function add_to_layers(prop_string, expr){    var error_array = [];    var prop_array = prop_string.split(".");    var sel_layers = comp.selectedLayers;    for (i = 0; i < sel_layers.length; i++)    {        error_layer = build_eval_string(sel_layers[i], prop_array, expr)                 if (error_layer  != null)        {            error_array.push(error_layer)        }    }    if (error_array.length > 0)    {        alert("Could not add expression to "+error_array.length+" layer(s)\nMake sure the specified properties exists");    }}var exp = 'thisComp.layer("apa").effect("slider")("Slider")';//var exp = "time*10";add_to_layers("Effects.Circle.Radius", exp);//build_eval_string(null_ctrl, ["Effects", "Circle", "Radius"], "time*100")
