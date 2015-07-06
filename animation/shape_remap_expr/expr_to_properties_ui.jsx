4c7ee283999ffd9184635ef8c1cd72cc70199e02
Johan Borgström <johan@petfactory.se>
2015-07-07 01:23:51 +0200

    fix layout

--- animation/shape_remap_expr/expr_to_properties_ui.jsx
+++ animation/shape_remap_expr/expr_to_properties_ui.jsx
@@ -1,174 +1,1 @@
-function build_eval_string(layer, prop_array, expr)
-{
-    var layer = layer;
-    var eval_string = 'layer';
-
-    for (j = 0; j < prop_array.length; j++)
-    {
-        eval_string += '.property("'+prop_array[j]+'")';
-    }
-    eval_string += ".expression = \'"+ expr +"\'";
-    //alert(eval_string)
-    
-    try
-    {
-        eval(eval_string)
-        return null;
-    }
-    catch(e)
-    {
-        return layer;
-    }
-}
-
-function myScript(thisObj)
-{
-    function myScript_buildUI(thisObj)
-    {
-        var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "Add expr", [0, 0, 300, 300]);
-        var min_width = 180;
-        myPanel.alignChildren = ["fill",""];
-        myPanel.alignment = ['left', 'top'];
-        
-        myPanel.add ("statictext", undefined, "Replace:");
-        
-        var replace_0 = myPanel.add ("edittext", undefined, "");
-        replace_0.characters = 20;
-        replace_0.minimumSize.width = min_width;
-        
-        var replace_1 = myPanel.add ("edittext", undefined, "");
-        replace_1.characters = 20;
-        replace_1.minimumSize.width = min_width;
-        
-        var replace_2 = myPanel.add ("edittext", undefined, "");
-        replace_2.characters = 20;
-        replace_2.minimumSize.width = min_width;
-        
-        var replace_3 = myPanel.add ("edittext", undefined, "");
-        replace_3.characters = 20;
-        replace_3.minimumSize.width = min_width;
-        
-        
-        myPanel.add ("statictext", undefined, "Expression:");
-        
-        add_ctrl_button = myPanel.add ("button", undefined, "Add expression ctrl string");
-        add_ctrl_button.minimumSize.width = min_width;
-        add_ctrl_button.onClick = function()
-        {
-            var comp = app.project.activeItem;     
-            if (comp == null) {alert("Select a comp!"); return;}
-            
-            var sel_layers = comp.selectedLayers;
-            if (sel_layers.length < 1) {alert("Nothing is selected!"); return;}
-            
-            var sel_properties = comp.selectedProperties;
-            if (sel_properties.length < 1) {alert("Select a property!"); return;}
-            
-            prop_grp_name = sel_properties[0].name
-            // index 1 will give us the name, 2 the compositing options
-            prop_name = sel_properties[0].property(1).name
-
-            ctrl_string = "thisComp.layer(\""+sel_layers[0].name+"\").effect(\""+prop_grp_name+"\")(\""+prop_name+"\")";
-            //alert(ctrl_string)
-            /*
-            if (exp_edittext.text.length < 1) exp_edittext.text = ctrl_string;
-            else alert("The edit text is not empty\nRemove all text and try again.")
-            */
-            var ctrl_string = "\"test\"";
-            system.callSystem("echo " +  ctrl_string + " | pbcopy");
-
-        }
-            
-        var exp_edittext = myPanel.add ("edittext", [0, 0, 200, 70], "", {multiline: true, scrolling: true, wantReturn: true});
-        //var exp_edittext = myPanel.add ("edittext", undefined, "");
-        exp_edittext.characters = 20;
-        exp_edittext.minimumSize.width = min_width;
-        
-        myPanel.add ("statictext", undefined, "Property:");
-        
-        var prop_edittext = myPanel.add ("edittext", undefined, "");
-        prop_edittext.characters = 20;
-        prop_edittext.minimumSize.width = min_width;
-        
-        add_exp_button = myPanel.add ("button", undefined, "Add expression");
-        add_exp_button.minimumSize.width = min_width;
-        add_exp_button.onClick = function()
-        {
-            
-            var comp = app.project.activeItem;
-            if (comp == null || !(comp instanceof CompItem)) { alert("Select a comp!"); return; }
-            
-            var exp_raw_text = exp_edittext.text;
-            var property_string = prop_edittext.text;
-            var property_array = property_string.split(".");
-            var sel_layers = comp.selectedLayers;
-            
-            if (sel_layers.length < 1) { alert('Nothing is selected!'); return;}
-                        
-            var error_array = [];
-            
-            for (i = 0; i < sel_layers.length; i++)
-            {
-                //var exp_string = exp_raw_text;
-                // total hack to remove the slanted quotationmark, look into this...
-                var exp_string = exp_raw_text.replace("\”", "\"");
-                
-                exp_string = exp_string.replace('{0}', eval(replace_0.text.replace("i", i)));
-                exp_string = exp_string.replace('{1}', eval(replace_1.text.replace("i", i)));
-                exp_string = exp_string.replace('{2}', eval(replace_2.text.replace("i", i)));
-                exp_string = exp_string.replace('{3}', eval(replace_3.text.replace("i", i)));
-                
-                //alert(exp_string)
-                
-                error_layer = build_eval_string(sel_layers[i], property_array, String(exp_string)) 
-                if (error_layer  != null) error_array.push(error_layer)
-                
-                //var property_string = prop_edittext.text;                  
-                //add_to_layers(comp, property_string, exp_string);
-            }
-            
-            if (error_array.length > 0)
-            {
-                alert("Could not add expression to "+error_array.length+" layer(s)\nMake sure the specified properties exists");
-            }
-
-        }
-
-        remove_exp_button = myPanel.add ("button", undefined, "Remove expression");
-        remove_exp_button.minimumSize.width = min_width;
-        
-        remove_exp_button.onClick = function()
-        {
-            if (!confirm("Remove expressions from selected layers?")) return;
-            
-            var comp = app.project.activeItem;
-            if (comp == null || !(comp instanceof CompItem)) { alert("Select a comp!"); return; }
-            
-            var property_string = prop_edittext.text;
-            var property_array = property_string.split(".");
-            var sel_layers = comp.selectedLayers;
-            for (i = 0; i < sel_layers.length; i++)
-            {
-                error_layer = build_eval_string(sel_layers[i], property_array, "");
-            }
-        }
-
-        //Setup panel sizing and make panel resizable
-        myPanel.layout.layout(true);
-        myPanel.layout.resize();
-        myPanel.onResizing = myPanel.onResize = function () {this.layout.resize();}
-
-        return myPanel;
-    }
-
-
-    var myScriptPal = myScript_buildUI(thisObj);
- 
-    if ((myScriptPal != null) && (myScriptPal instanceof Window))
-    {
-        myScriptPal.center();
-        myScriptPal.show();
-    }
-}
-      
-myScript(this);
+﻿function build_eval_string(layer, prop_array, expr){    var layer = layer;    var eval_string = 'layer';    for (j = 0; j < prop_array.length; j++)    {        eval_string += '.property("'+prop_array[j]+'")';    }    eval_string += ".expression = \'"+ expr +"\'";    //alert(eval_string)        try    {        eval(eval_string)        return null;    }    catch(e)    {        return layer;    }}function replace_editfield(parent, label){    var replace_0_grp = parent.add ("group");            var o = replace_0_grp.add ("statictext", undefined, label);    o.maximumSize.width = 20;    o.minimumSize.width = 20;    var replace_0 = replace_0_grp.add ("edittext", undefined, "");    replace_0.characters = 20;        replace_0.alignment =  ["fill",""];        return replace_0;}function myScript(thisObj){    function myScript_buildUI(thisObj)    {        var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "Add expr", [0, 0, 300, 300]);        var min_width = 120;        myPanel.alignChildren = ["fill",""];                var propety_panel = myPanel.add("panel", undefined, "Property");        propety_panel.alignment = ["fill","top"];        propety_panel.alignChildren = ["fill",""];                var prop_edittext = propety_panel.add ("edittext", undefined, "");        prop_edittext.characters = 20;        prop_edittext.minimumSize.width = min_width;                var replace_panel = myPanel.add("panel", undefined, "Replace");        replace_panel.alignChildren = ["fill",""];        replace_panel.alignment = ["fill","top"];                var replace_0 = replace_editfield(replace_panel, "{0}");        var replace_1 = replace_editfield(replace_panel, "{1}");        var replace_2 = replace_editfield(replace_panel, "{2}");        var replace_3 = replace_editfield(replace_panel, "{3}");         var expr_panel = myPanel.add("panel", undefined, "Expression");        expr_panel.alignment = ["fill","fill"];                add_ctrl_button = expr_panel.add ("button", undefined, "Copy ctrl string");        add_ctrl_button.alignment = ["fill","top"];        add_ctrl_button.onClick = function()        {            var comp = app.project.activeItem;                 if (comp == null) {alert("Select a comp!"); return;}                        var sel_layers = comp.selectedLayers;            if (sel_layers.length < 1) {alert("Nothing is selected!"); return;}                        var sel_properties = comp.selectedProperties;            if (sel_properties.length < 1) {alert("Select a property!"); return;}                        prop_grp_name = sel_properties[0].name            // index 1 will give us the name, 2 the compositing options            prop_name = sel_properties[0].property(1).name            ctrl_string = "thisComp.layer(\""+sel_layers[0].name+"\").effect(\""+prop_grp_name+"\")(\""+prop_name+"\")";            //alert(ctrl_string)            /*            if (exp_edittext.text.length < 1) exp_edittext.text = ctrl_string;            else alert("The edit text is not empty\nRemove all text and try again.")            */            var ctrl_string = "\"test\"";            system.callSystem("echo " +  ctrl_string + " | pbcopy");        }                    var exp_edittext = expr_panel.add ("edittext", [0, 0, 200, 70], "", {multiline: true, scrolling: true, wantReturn: true});        exp_edittext.alignment = ["fill","fill"];                var button_grp = myPanel.add("group", undefined);        button_grp.alignment = ["fill","bottom"];        button_grp.alignChildren = ["fill",""];                remove_exp_button = button_grp.add ("button", undefined, "Remove expression");        remove_exp_button.minimumSize.width = min_width;                remove_exp_button.onClick = function()        {            if (!confirm("Remove expressions from selected layers?")) return;                        var comp = app.project.activeItem;            if (comp == null || !(comp instanceof CompItem)) { alert("Select a comp!"); return; }                        var property_string = prop_edittext.text;            var property_array = property_string.split(".");            var sel_layers = comp.selectedLayers;            for (i = 0; i < sel_layers.length; i++)            {                error_layer = build_eval_string(sel_layers[i], property_array, "");            }        }                add_exp_button = button_grp.add ("button", undefined, "Add expression");        add_exp_button.minimumSize.width = min_width;        add_exp_button.onClick = function()        {                        var comp = app.project.activeItem;            if (comp == null || !(comp instanceof CompItem)) { alert("Select a comp!"); return; }                        var exp_raw_text = exp_edittext.text;            var property_string = prop_edittext.text;            var property_array = property_string.split(".");            var sel_layers = comp.selectedLayers;                        if (sel_layers.length < 1) { alert('Nothing is selected!'); return;}                                    var error_array = [];                        for (i = 0; i < sel_layers.length; i++)            {                //var exp_string = exp_raw_text;                // total hack to remove the slanted quotationmark, look into this...                var exp_string = exp_raw_text.replace("\”", "\"");                                exp_string = exp_string.replace('{0}', eval(replace_0.text.replace("i", i)));                exp_string = exp_string.replace('{1}', eval(replace_1.text.replace("i", i)));                exp_string = exp_string.replace('{2}', eval(replace_2.text.replace("i", i)));                exp_string = exp_string.replace('{3}', eval(replace_3.text.replace("i", i)));                                //alert(exp_string)                                error_layer = build_eval_string(sel_layers[i], property_array, String(exp_string))                 if (error_layer  != null) error_array.push(error_layer)                                //var property_string = prop_edittext.text;                                  //add_to_layers(comp, property_string, exp_string);            }                        if (error_array.length > 0)            {                alert("Could not add expression to "+error_array.length+" layer(s)\nMake sure the specified properties exists");            }        }        //Setup panel sizing and make panel resizable        myPanel.layout.layout(true);        myPanel.layout.resize();        myPanel.onResizing = myPanel.onResize = function () {this.layout.resize();}        return myPanel;    }    var myScriptPal = myScript_buildUI(thisObj);     if ((myScriptPal != null) && (myScriptPal instanceof Window))    {        myScriptPal.center();        myScriptPal.show();    }}      myScript(this);
